4adc9f2d04fc76e8353558fde345e1d8ff4daeb6
Umarnaas <158535442+Umarnaas@users.noreply.github.com>
2024-02-04 00:11:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
+global.owner = [Omarnaas]
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "" //2347053583031
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
